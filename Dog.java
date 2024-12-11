78fd03931aebb2f42d467aa85d290f1be7052b56
khangv9342 <kmvo100@student.hbuhsd.edu>
2024-12-11 10:16:04 -0800

    sefe

--- Dog.java
+++ Dog.java
@@ -1,4 +1,33 @@
 public class Dog
 {
+private String breed;
+private String color;
+private double size;
 
+public String getBreed()
+{
+    return breed;
+}
+public String getColor()
+{
+    return color;
+}
+public double getSize()
+{
+    return size;
+}
+
+
+public void setBreed(String iBreed)
+{
+    breed = iBreed;
+}
+public void setColor(String iColor)
+{
+    color = iColor;
+}
+public void setSize(String iSize)
+{
+    size = iSize;
+}
 }
